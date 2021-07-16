2950630d9ca43bf1bb74419256dca3c3ac554347
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-07-16 22:00:11 +0900

    basic_test_level2

--- visit_length.py
+++ visit_length.py
@@ -53,7 +53,101 @@ def solution(dirs):
     
     return int(count)
 
-print(solution('ULURRDLLU'))
-print(solution('LULLLLLLU'))
+#print(solution('ULURRDLLU'))
+#print(solution('LULLLLLLU'))
 
 ###############################################################
+
+'''
+ 통과
+
+'''
+
+def solution2(dirs):
+    move = {'U' : (0, 1), 'D' : (0, -1), 'R' : (1, 0), 'L' : (-1, 0)}
+    x, y = 0, 0 # 처음 위치(x0, y0)
+    length = set()
+    
+    for i in dirs:
+        x1 = move[i][0] # x방향의 거리 변화량(dx)
+        y1 = move[i][1] # y방향의 거리 변화량(dy)
+        x2 = x + x1 # x의 나중 위치 -> x = x0 + dx
+        y2 = y + y1 # y의 나중 위치 -> y = y0 + dy
+        if -5 <= x2 <= 5 and -5 <= y2 <= 5:
+            length.add((x, y, x2, y2)) # 이동하는 경로
+            #length.add((x2, y2, x, y)) # 되돌아가는 경로
+            x, y = x2, y2 # 현재 위치를 리셋 해준다. -> x, y가 이동전 위치를 가지므로,
+                          # 그 값을 이동 후 위치로 바꿔서 for문을 돌린다.
+                          # if 조건문 안에 위치한 이유는 좌표 전체의 크기가 (5x5)크기 이기 때문에
+                          # 그 값을 넘어갈 수 없기 때문.
+    
+    return len(length) // 2
+
+#print(solution2('ULURRDLLU'))
+#print(solution2('LULLLLLLU'))
+    
+    
+####################################################################
+'''
+ 실패
+'''
+
+def solution3(dirs):
+    move = {'U' : (0, 1), 'D' : (0, -1), 'R' : (1, 0), 'L' : (-1, 0)}
+    length = set()
+    x, y = 0, 0
+    
+    for i in dirs:
+        x1 = move[i][0]
+        y1 = move[i][1]
+        x2 = x + x1
+        y2 = x + y1
+        if -5 <= x2 <= 5 and -5 <= y2 <= 5:
+            a = (x, y, x2, y2)
+            b = (x2, y2, x, y)
+            x, y = x2, y2
+        else:
+            x2 -= x1
+            y2 -= y1
+        
+        if a not in length:
+            length.add(a)
+            length.add(b)
+            
+    return len(length) // 2
+    
+    
+    
+##################################################################
+'''
+ 통과
+'''
+
+
+def solution4(dirs):
+    command = {'U':(0, 1), 'D':(0, -1), 'L':(-1, 0), 'R':(1, 0)}
+    road = set()
+    cur_x, cur_y = (0,0)
+    
+    for d in dirs:
+        next_x, next_y = cur_x + command[d][0], cur_y + command[d][1]
+        if -5<= next_x <=5 and -5<= next_y <=5:
+            road.add((cur_x, cur_y, next_x, next_y))
+            road.add((next_x, next_y, cur_x, cur_y))
+            cur_x, cur_y = next_x, next_y
+
+    return len(road) // 2
+    
+    
+    
+print(solution4('ULURRDLLU'))
+print(solution4('LULLLLLLU'))    
+    
+    
+    
+    
+    
+    
+    
+    
+    
