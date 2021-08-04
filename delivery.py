8c755ed879006c4c879d3e2faf37b1cad0c9e005
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-08-04 21:53:09 +0900

    basic_test_level2

--- delivery.py
+++ delivery.py
@@ -42,13 +42,59 @@ def solution(N, road, K):
 
     return dic
 
-print(solution(6, [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]], 4))
+#print(solution(6, [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]], 4))
 
 #########################################################################
 
 '''
  2차
+ - 다익스트라.
+ 인터넷 참조(https://post.naver.com/viewer/postView.nhn?volumeNo=26748173&memberNo=33264526)
 '''
+import math
+from collections import deque
+
+def bfs(start, maps, distance, K):
+    queue = deque()
+    queue.append(start)
+    
+    distance[start] = 0
+    
+    while queue:
+        y = queue.popleft()
+        for x in range(1, len(maps)):
+            if maps[y][x] != 0:
+                if distance[x] > distance[y] + maps[y][x] and distance[y] + maps[y][x] <= K:
+                    distance[x] = distance[y] + maps[y][x]
+                    queue.append(x)
+    
+    return len([i for i in distance if i<= K])
+
+def solution2(N, road, K):
+    distance = [math.inf for _ in range(N + 1)]
+    
+    maps = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+    
+    for fro, to ,w in road:
+        if maps[fro][to] == 0 and maps[to][fro] == 0:
+            maps[fro][to], maps[to][fro] = w, w
+        else:
+            if w < maps[fro][to]:
+                maps[fro][to], maps[to][fro] = w, w
+                
+    return bfs(1, maps, distance, K)
+
+
+print(solution2(6, [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]], 4))
+
+
+
+
+
+
+
+
+
 
 
 
